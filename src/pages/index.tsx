4f1d664a72de3bfe5c3c02099f9e14abc7175a52
Miftah Fauzy <miftahfauzy79@gmail.com>
2024-11-13 23:12:51 +0700

    trial deploy pt 16

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -477,8 +477,17 @@ export default function Home() {
               <p className="mt-2 font-sans text-sm">Rumi Properti adalah platform untuk mencari hunian idamanmu.  Dengan menampilkan pilihan properti terjangkau, Anda bisa mendapatkan hunian yang nyaman dan tentunya sesuai dengan budget Anda.</p>
             </div>
             <div className="w-full h-full bg-gray-300 rounded-md relative">
-              <iframe className="absolute top-0 left-0 w-full h-full rounded-md" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3966.3258848578675!2d106.7735920749904!3d-6.220687993767329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTMnMTQuNSJTIDEwNsKwNDYnMzQuMiJF!5e0!3m2!1sid!2sid!4v1731511353384!5m2!1sid!2sid" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              <iframe
+                className="absolute top-0 left-0 w-full h-full rounded-md"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3966.3258848578675!2d106.7735920749904!3d-6.220687993767329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTMnMTQuNSJTIDEwNsKwNDYnMzQuMiJF!5e0!3m2!1sid!2sid!4v1731511353384!5m2!1sid!2sid"
+                width="100%"
+                height="100%"
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
+
           </div>
         </footer>
       </div >
