72e43045f50b514ec5421a112cb3b62078e0f5da
Miftah Fauzy <miftahfauzy79@gmail.com>
2024-11-24 11:16:56 +0700

    add link to developer page, add slug developer page, add navbar and footer to developer page, add snippet dark mode to developer page

--- src/pages/developer/[slug].tsx
+++ src/pages/developer/[slug].tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { useDeveloper } from "../api/fetchAPI";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+const Footer = dynamic(() => import('src/component/footer'), { ssr: false });
+const Navbar = dynamic(() => import('src/component/navbar'), { ssr: false });
 
 interface Developer {
     id: number;
@@ -37,22 +40,28 @@ const DeveloperDetailPage = () => {
     }
 
     return (
-        <div className="container mx-auto py-10 px-4">
-            <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6">
-                {currentDeveloper.logoDeveloper?.formats?.thumbnail?.url && (
-                    <div className="w-40 h-40 rounded-full overflow-hidden mx-auto mb-6">
-                        <Image
-                            src={currentDeveloper.logoDeveloper.formats.thumbnail.url}
-                            alt={currentDeveloper.nameDeveloper}
-                            width={160}
-                            height={160}
-                            className="object-cover"
-                        />
+        <div>
+            <Navbar />
+            <main className="dark:bg-[#1E1E1E]">
+                <div className="container mx-auto py-10 px-4">
+                    <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6">
+                        {currentDeveloper.logoDeveloper?.formats?.thumbnail?.url && (
+                            <div className="w-40 h-40 rounded-full overflow-hidden mx-auto mb-6">
+                                <Image
+                                    src={currentDeveloper.logoDeveloper.formats.thumbnail.url}
+                                    alt={currentDeveloper.nameDeveloper}
+                                    width={160}
+                                    height={160}
+                                    className="object-cover"
+                                />
+                            </div>
+                        )}
+                        <h1 className="text-2xl font-bold text-center mb-4">{currentDeveloper.nameDeveloper}</h1>
+                        <p className="text-gray-700 text-justify">{currentDeveloper.aboutDeveloper}</p>
                     </div>
-                )}
-                <h1 className="text-2xl font-bold text-center mb-4">{currentDeveloper.nameDeveloper}</h1>
-                <p className="text-gray-700 text-justify">{currentDeveloper.aboutDeveloper}</p>
-            </div>
+                </div>
+            </main>
+            <Footer />
         </div>
     );
 };
