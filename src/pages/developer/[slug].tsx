41e2a1478464f97d0143dbdadbd5029899795748
Miftah Fauzy <miftahfauzy79@gmail.com>
2024-11-24 15:36:03 +0700

    split text for developer title and descriptions

--- src/pages/developer/[slug].tsx
+++ src/pages/developer/[slug].tsx
@@ -111,7 +111,7 @@ const DeveloperDetailPage = () => {
         <div>
             <Navbar />
             <main className="dark:bg-[#1E1E1E]">
-                <div className="container mx-auto py-10 px-4">
+                <div className="container mx-auto flex flex-col gap-5 py-10 px-4">
                     <div className="max-w-2xl mx-auto bg-white dark:border-none dark:bg-[#252525] rounded-lg shadow-lg p-6">
                         {currentDeveloper.logoDeveloper?.formats?.thumbnail?.url && (
                             <div className="relative w-40 h-40 rounded-full overflow-hidden mx-auto mb-6">
@@ -128,6 +128,10 @@ const DeveloperDetailPage = () => {
                             </div>
                         )}
                         <h1 className="text-2xl font-bold text-center mb-4 dark:text-white">{currentDeveloper.nameDeveloper}</h1>
+                        <p className="text-gray-700 dark:text-[#757575] text-justify">{currentDeveloper.aboutDeveloper}</p>
+                    </div>
+                    <div className="max-w-2xl mx-auto bg-white dark:border-none dark:bg-[#252525] rounded-lg shadow-lg p-6">
+                        <h2 className="font-medium text-[#24221D] dark:text-white">Tentang Developer</h2>
                         <p className="text-gray-700 dark:text-[#757575] text-justify">{currentDeveloper.aboutDeveloper}</p>
                     </div>
                 </div>
