64c58bc07c6c5115c1c68c49ae901a401bc45eee
Miftah Fauzy <miftahfauzy79@gmail.com>
2024-12-15 12:51:43 +0700

    fix image of Property banner

--- src/pages/sewa.tsx
+++ src/pages/sewa.tsx
@@ -259,8 +259,8 @@ export default function Property() {
                                         <div className="w-full h-[200px] overflow-hidden">
                                             <Image
                                                 className="w-full h-full object-cover hover:scale-110 transition"
-                                                src={item.bannerProperty?.formats?.thumbnail.url ||
-                                                    item.bannerProperty?.formats?.large.url ||
+                                                src={item.bannerProperty?.formats?.large.url ||
+                                                    item.bannerProperty?.formats?.thumbnail.url ||
                                                     item.bannerProperty?.formats?.medium?.url ||
                                                     item.bannerProperty?.formats?.small.url ||
                                                     ''}
