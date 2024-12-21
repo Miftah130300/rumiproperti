1a41ceeb4982fe72f9659d78381ac0543f0631ca
Miftah Fauzy <miftahfauzy79@gmail.com>
2024-12-21 18:22:24 +0700

    add blur image property part 26

--- src/component/carouselProperti/carouselProperti.tsx
+++ src/component/carouselProperti/carouselProperti.tsx
@@ -50,18 +50,16 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
                             className="embla__slide-property flex items-center justify-center w-full h-full relative overflow-hidden"
                             key={index}
                         >
+                            {/* Gambar utama */}
                             <Image
                                 src={src}
                                 alt={`Slide ${index}`}
                                 loader={loader}
                                 className="h-auto w-auto max-h-full max-w-full object-contain z-10"
                             />
-                            <Image
-                                src={src}
-                                alt={`Slide ${index} background`}
-                                loader={loader}
-                                className="absolute inset-0 w-full h-full object-cover blur-md scale-110"
-                            />
+
+                            {/* Overlay blur */}
+                            <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-lg z-0"></div>
                         </div>
                     ))}
                 </div>
